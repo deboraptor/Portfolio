a4155f2d8d497c557c607b2c90317afa8ab8d67a
deboraptor <vdzdebora@gmail.com>
2026-01-16 18:25:03 +0100

    :truck: move JS file into folder and change html files' paths

--- about.js
+++
@@ -1,5 +0,0 @@
-/* TODO: revoir, ça marche po trop */
-        var largeur = frame.innerWidth;
-        var hauteur = frame.innerHeight;
-        document.getElementById("cadre").style.width = (largeur) + "px";
-        document.getElementById("cadre").style.height = (hauteur) + "px";
